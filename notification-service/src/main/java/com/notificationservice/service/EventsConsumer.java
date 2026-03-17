948415889a7c8b7327f8f8b9c9d5a8d3de0a84ba
MOHAMED EL AFIA <mohamedelafia723@gmail.com>
2026-03-17 23:05:16 +0000

    notification service changes" ; exit "

--- notification-service/src/main/java/com/notificationservice/service/EventsConsumer.java
+++ notification-service/src/main/java/com/notificationservice/service/EventsConsumer.java
@@ -3,16 +3,18 @@ package com.notificationservice.service;
 
 import com.notificationservice.enums.NotificationEventType;
 import com.notificationservice.events.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventsConsumer {
 
+    @Autowired
+    private MailService mailService;
+
     @KafkaListener(topics = "notifications-topic" , groupId="notification-group")
     public void consume(NotificationEvent notificationEvent){
-        if(notificationEvent.getNotificationEventType() == NotificationEventType.TRANSACTION_EVENT)  {
-
-        }
+        this.mailService.sendMail(notificationEvent.getEmail(),"Banking notification", notificationEvent.getMessage());
     }
 }
---
+++ notification-service/src/main/java/com/notificationservice/service/MailService.java
@@ -0,0 +1,27 @@
+package com.notificationservice.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender mailSender;
+    @Value("${config.mail.username}")
+    private static String FROM;
+
+    public void sendMail(String to ,String subject , String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(FROM) ;
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+
+        mailSender.send(message);
+    }
+}
